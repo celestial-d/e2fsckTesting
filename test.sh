bbb5a2f18918ea96b6cec25fe70b17b3a53614a3
Duo Zhang <37310481+celestial-d@users.noreply.github.com>
2022-09-01 11:22:56 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,12 +10,17 @@ do
 	echo "####################################"
 	echo $c
 	echo "############################### "
-	echo ''
+	echo "!!!!dd if=/dev/zero of=test.img bs=1k count=100000!!!!"
 	dd if=/dev/zero of=test.img bs=1k count=100000
-	eval $c
-	sleep 1
-	e2fsck -y test.img
-	resize2fs -f test.img 500M
-	e2fsck -y test.img
+	echo "!!!!mkfe2fs!!!!"
+	if eval $c; then
+		sleep 1
+		echo "!!!!e2fsck !!!!"
+		e2fsck -y test.img
+		echo "!!!!resize2fs -f test.img 500M!!!!"
+		resize2fs -f test.img 500M
+		echo "!!!!e2fsck !!!!"
+		e2fsck -y test.img
+	fi
 	rm test.img
 done	
